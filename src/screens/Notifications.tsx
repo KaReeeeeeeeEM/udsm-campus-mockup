1cdf5e9444e853eda16a246ea28ee586e968e04d
KaReeeeeeeeEM <s2kizzyjunior13052002@gmail.com>
2025-02-13 05:03:46 +0300

    feat: added notifications in the notifications tab

--- src/screens/Notifications.tsx
+++ src/screens/Notifications.tsx
@@ -3,6 +3,8 @@ import Profile from "../components/demo/Avatar";
 import { MdNotifications, MdTv } from "react-icons/md";
 import Notification from "../components/demo/Notification";
 import { suggestions } from "../data";
+import { CgUserAdd } from "react-icons/cg";
+import { BiCheck, BiCheckCircle } from "react-icons/bi";
 
 export default function Notifications() {
   return (
@@ -41,7 +43,35 @@ export default function Notifications() {
       </div>
 
       <div>
-        
+        <div className="bg-neutral-700 hover:bg-neutral-600 rounded-lg w-full p-2 flex flex-col mt-2 mb-3">
+            <div className="flex justify-between items-center">
+                <span className="text-blue-400 text-xs flex items-center gap-2"> <CgUserAdd /> Today</span>
+                <span className="text-neutral-400 text-xs">10:30 AM</span>
+            </div>
+            <span className="text-neutral-400 text-xs">
+              Jabari Kwesa wants to Connect.
+            </span>
+        </div>
+
+        <div className="bg-neutral-700 hover:bg-neutral-600 rounded-lg w-full p-2 flex flex-col mt-2 mb-3">
+            <div className="flex justify-between items-center">
+                <span className="text-blue-400 text-xs flex items-center gap-2"> <BiCheckCircle /> Today</span>
+                <span className="text-neutral-400 text-xs">08:33 AM</span>
+            </div>
+            <span className="text-neutral-400 text-xs">
+              Salama Bakari accepted your request.
+            </span>
+        </div>
+
+        <div className="bg-neutral-700 hover:bg-neutral-600 rounded-lg w-full p-2 flex flex-col mt-2 mb-3">
+            <div className="flex justify-between items-center">
+                <span className="text-blue-400 text-xs flex items-center gap-2"> <BiCheckCircle />Yesterday</span>
+                <span className="text-neutral-400 text-xs">23:12 PM</span>
+            </div>
+            <span className="text-neutral-400 text-xs">
+              Sophia Vedasto accepted your request.
+            </span>
+        </div>
       </div>
 
       {/* channels suggestions */}
